bb4f26d17d2989388750e1d40962ac26eb2f6a6d
Abdurrahman Tantawi <abdurrahman.tantawi@gmail.com>
2026-03-13 20:53:39 +0300

    setting up project, separating docs from app

---
+++ src/main/java/sa/abrahman/zaxeg/ZaxegApplication.java
@@ -0,0 +1,13 @@
+package sa.abrahman.zaxeg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaxegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaxegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sa/abrahman/zaxeg/ZaxegApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.abrahman.zaxeg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaxegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
