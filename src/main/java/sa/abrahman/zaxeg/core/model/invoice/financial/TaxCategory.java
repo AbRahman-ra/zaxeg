17c08d6ebbf2999e6d8e8804e0da8530ff45dfe3
Abdurrahman Tantawi <abdurrahman.tantawi@gmail.com>
2026-03-18 06:52:04 +0300

    ref: renaming packages, feat: ZATCA phase I done so far

---
+++ src/main/java/sa/abrahman/zaxeg/core/model/invoice/financial/TaxCategory.java
@@ -0,0 +1,18 @@
+package sa.abrahman.zaxeg.core.model.invoice.financial;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TaxCategory {
+    STANDARD("S", new BigDecimal("15.00")),
+    ZERO_RATED("Z", new BigDecimal("0.00")),
+    EXEMPT("E", new BigDecimal("0.00")), // Healthcare, real estate, etc.
+    OUT_OF_SCOPE("O", new BigDecimal("0.00")); // Out of KSA tax scope
+
+    private final String code;
+    private final BigDecimal rate;
+}
